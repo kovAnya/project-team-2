4a0172bdb50653370802d476bbc980e78f868ff6
Olha Chornei <pradocw8053@gmail.com>
2022-10-09 19:03:53 +0300

    delete folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // import './js/modal_film';
-// import './js/pagination';
+import './js/pagination';
 import { renderMoviesTrending } from './js/renderMoviesTrending';
 import { fetchMoviesTrending } from './js/fetchMoviesTrending';
-import {page} from './js/refs';
+import { page } from './js/refs';
 import './js/spinner';
 import './js/modal-log-in';
 import './js/log-in';
@@ -10,8 +10,3 @@ import './js/footer-modal';
 import './js/genres';
 
 renderMoviesTrending(fetchMoviesTrending(page));
-
-
-
-
-
