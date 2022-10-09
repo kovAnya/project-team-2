4d4ecd8bc5482e5649b3acd63c6597107f952536
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-10-09 14:43:48 +0300

    add_spinner_commit

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,10 +1,10 @@
 
-
+import { loader } from './refs';
 window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
     loader.classList.add("loader-hidden");
     loader.addEventListener("transitionend", () => {
         document.body.removeChild(".loader");
     })
 })
+    
 
