f2f9f85cd6ecc1bb7a44037dbbcedfc8b4a60da5
Olha Chornei <pradocw8053@gmail.com>
2022-10-09 21:28:37 +0300

    add new file

--- src/js/renderMoviesTrending.js
+++ src/js/renderMoviesTrending.js
@@ -1,6 +1,6 @@
-import { fetchMoviesTrending } from "./fetchMoviesTrending";
-import {card} from "./oneCard";
-import {getMovieGenres} from "./genres"
+import { fetchMoviesTrending } from './fetchMoviesTrending';
+import { card } from './oneCard';
+import { getMovieGenres } from './genres';
 
 ///////////////Елемент одной карточки в HTML документе
 const moviesElement = document.querySelector('.movies');
@@ -8,51 +8,52 @@ const moviesElement = document.querySelector('.movies');
 
 ////////////////Путь и размер запроса картинок
 let BASE_URL_IMAGE = 'https://image.tmdb.org/t/p';
-let fileSize = 'original';
+let fileSize = 'w400';
 ///////////////////////////////////////////////
 
 ///////////////Функция рендеринга 1 карточки фильма
-export   async function renderMoviesTrending(dataFromServer){
-        try{
-            let data = await dataFromServer;
-            let arrsFilm = data.results ////Обьект с 20 фильмами
+export async function renderMoviesTrending(dataFromServer) {
+  try {
+    let data = await dataFromServer;
+    let arrsFilm = data.results; ////Обьект с 20 фильмами
 
-            
-            arrsFilm.map(
-                ({poster_path, title, name, genre_ids, release_date, first_air_date}) =>      ////Перебираем каждый фильм и берем данные
-                  {    
-            let poster = `${BASE_URL_IMAGE}/${fileSize}/${poster_path}`;  ////Картинка фильма
-            let genre = [];  /// Жанры фильма
-            let releasedYear =""; /// Год релиза
-            let cardFilm = "";  /// Обьявление переменной для карточки фильма
+    arrsFilm.map(
+      (
+        { poster_path, title, name, genre_ids, release_date, first_air_date } ////Перебираем каждый фильм и берем данные
+      ) => {
+        let poster = `${BASE_URL_IMAGE}/${fileSize}/${poster_path}`; ////Картинка фильма
+        let genre = []; /// Жанры фильма
+        let releasedYear = ''; /// Год релиза
+        let cardFilm = ''; /// Обьявление переменной для карточки фильма
 
-            if(release_date){                             /// Если в данных есть значение release_date то данный год берем с него
-                let ReleasedData = release_date.split("")
-                releasedYear = ReleasedData.splice(0, 4).join("");
-            } else {
-                let ReleasedData = first_air_date.split("")     /// Если нет release_date то берем год с first_air_date
-                releasedYear = ReleasedData.splice(0, 4).join("");
-            }
-
-            for(const ids of genre_ids) {
-                if(getMovieGenres(ids)){              /// Если id жанра есть в списке жанров то добавляем в карточку, если нет то не добавляем
-                genre.push(` ${getMovieGenres(ids)}`)
-                }
-             }
-
-
-            if(title) {                      /// Название фильма берем с title 
-             cardFilm = card(poster, title, genre, releasedYear);
-            } else {
-             cardFilm = card(poster, name, genre, releasedYear)   //// Если нет title в данных то берем с name
-            }
-             moviesElement.insertAdjacentHTML("beforeend", cardFilm);          /// Добавляем сформированную карточку в HTML код
-
-            })
-            
-        } catch(error) {
-            console.log(error.message)
+        if (release_date) {
+          /// Если в данных есть значение release_date то данный год берем с него
+          let ReleasedData = release_date.split('');
+          releasedYear = ReleasedData.splice(0, 4).join('');
+        } else if (first_air_date) {
+          let ReleasedData = first_air_date.split(''); /// Если нет release_date то берем год с first_air_date
+          releasedYear = ReleasedData.splice(0, 4).join('');
+        } else {
+          releasedYear = '';
         }
-    }
 
-    
+        for (const ids of genre_ids) {
+          if (getMovieGenres(ids)) {
+            /// Если id жанра есть в списке жанров то добавляем в карточку, если нет то не добавляем
+            genre.push(` ${getMovieGenres(ids)}`);
+          }
+        }
+
+        if (title) {
+          /// Название фильма берем с title
+          cardFilm = card(poster, title, genre, releasedYear);
+        } else {
+          cardFilm = card(poster, name, genre, releasedYear); //// Если нет title в данных то берем с name
+        }
+        moviesElement.insertAdjacentHTML('beforeend', cardFilm); /// Добавляем сформированную карточку в HTML код
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+}
