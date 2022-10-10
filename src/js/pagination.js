e1642130266ed7195a77d1d53cd99640ac5b3335
suprunyuk-a <chaserenergy@gmail.com>
2022-10-10 19:48:28 +0300

    edit_darkTheme

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,30 +1,32 @@
-import Pagination from 'tui-pagination'
+import Pagination from 'tui-pagination';
 
-const options = { // below default value of options
-     totalItems: '',
-     itemsPerPage: 20,
-     visiblePages: 5,
-     page: 1,
-     centerAlign: true,
-     totalPages:"",
-     firstItemClassName: 'tui-first-child',
-     lastItemClassName: 'tui-last-child',
-     template: {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
+const options = {
+  // below default value of options
+  totalItems: '',
+  itemsPerPage: 20,
+  visiblePages: 7,
+  page: 2,
+  centerAlign: true,
+  totalPages: '',
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 
 options.totalPages = 1000;
@@ -34,5 +36,3 @@ const container = document.getElementById('tui-pagination-container');
 const pagination = new Pagination(container, options);
 
 pagination.getCurrentPage();
-
-
