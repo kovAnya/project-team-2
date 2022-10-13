0480addd11a6dcf37ccfa221c141b8cb42e1fa0f
suprunyuk-a <chaserenergy@gmail.com>
2022-10-13 23:14:23 +0300

    add_pagination

--- src/js/fetchMoviesTrending.js
+++ src/js/fetchMoviesTrending.js
@@ -1,19 +1,31 @@
 const axios = require('axios').default;
-import { ApiKey } from "./refs";
+import { pagination } from './pagination';
+import { ApiKey } from './refs';
+import { renderMoviesTrending } from './renderMoviesTrending';
+import { onScroll, onToTopBtn } from './scroll';
 
 const URL = 'https://api.themoviedb.org/3/trending';
+const moviesElement = document.querySelector('.movies');
 
 let mediaType = 'all';
 let timeWindow = 'day';
 
+export async function fetchMoviesTrending(page) {
+  try {
+    const responce = await axios.get(
+      `${URL}/${mediaType}/${timeWindow}?api_key=${ApiKey}&page=${page}`
+    );
 
-export async function fetchMoviesTrending (page) {
-    
-try{
-    const responce = await axios.get(`${URL}/${mediaType}/${timeWindow}?api_key=${ApiKey}&page=${page}`);
     return responce.data.results;
-} catch (error) {
+  } catch (error) {
     console.error(error);
+  }
 }
 
-}
+pagination.on('afterMove', event => {
+  const currentPage = event.page;
+  moviesElement.innerHTML = '';
+  renderMoviesTrending(fetchMoviesTrending(currentPage));
+  onScroll();
+  onToTopBtn();
+});
