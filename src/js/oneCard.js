eb66612a80039df7e5e0856e9b185a3d3ff7e518
Anna Kovalchuk <104009473+kovAnya@users.noreply.github.com>
2022-10-16 00:12:38 +0300

    Update oneCard.js

--- src/js/oneCard.js
+++ src/js/oneCard.js
@@ -20,7 +20,7 @@ export function card(
             class="movie_cover lazyload"
             alt="${title}"
             data-id="${id}"
-            src ="../../images/placeholder/no-image_desktop.jpg"
+            src ="../src/images/placeholder/no-image_desktop.jpg"
           />`
             : ''
         }
