448842515270f0dbed92f77fbf2229e7049c6b71
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-10-15 13:01:42 +0300

    Добавил на инпут функционал
    Добавил на инпут функционал фильтрации поиска фильмов , но он не работает

--- src/js/search_movie.js
+++ src/js/search_movie.js
@@ -4,7 +4,9 @@ import { loader } from './refs';
 
 const axios = require('axios').default;
 
+import Notiflix from 'notiflix';
 
+import debounce from 'lodash.debounce';
 
 import { renderMoviesTrending } from './renderMoviesTrending';
 
@@ -13,7 +15,50 @@ const searchFilmInput = document.querySelector('.header__form-input');
 const moviesElement = document.querySelector('.movies');
 
 
+const DEBOUNCE_DELAY = 300;
+
 let inputValue = '';
+
+// Применяем приём Debounce (задержка в 300 мс)
+searchFilmInput.addEventListener('input',
+  debounce(onInputChenge, DEBOUNCE_DELAY));
+
+// Санитизация введённой строки методом trim()
+async function onInputChenge() {
+  const page = searchFilmInput.value.trim();
+  if (page === '') {
+    return (moviesElement.innerHTML = '');
+   
+  }
+
+  const data = await fetchFilms(page);
+  console.log(data);
+  moviesElement.innerHTML = '';
+  
+
+  try {
+    // Интерфейс с помощью бибилиотеки Notiflix
+    if (data.length > 10) {
+      Notiflix.Notify.info(
+        'Too many matches found. Please enter a more specific movie name.'
+      );
+    } else if (data.length < 10 && data.length >= 2) {
+      moviesElement.insertAdjacentHTML(
+        'beforeend',
+        card(data)
+      );
+    } else {
+      moviesElement.insertAdjacentHTML(
+        'beforeend',
+        card(data)
+      );
+    }
+  }
+  catch {
+    Notiflix.Notify.failure('Oops, there is no movie with that name');
+        return [];
+  }
+}
 
 
 async function fetchFilms(page) {
@@ -30,15 +75,17 @@ async function fetchFilms(page) {
 
 async function searchFilm(e) {
   e.preventDefault();
-   loader.classList.add('loader-hidden');
+
+
+    
+    
   inputValue = searchFilmInput.value;
   console.log(searchFilmInput.value);
   searchFilmForm.reset();
   moviesElement.innerHTML = '';
   await renderMoviesTrending(fetchFilms(page));
-  
+  loader.classList.add('loader-hidden');
 }
 
 searchFilmForm.addEventListener('submit', searchFilm);
-
 
