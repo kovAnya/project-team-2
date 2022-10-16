5b7d3ac7641b88c93d6506b332b76355377b9204
Yevdokymenko <102970897+Yevdokymenko@users.noreply.github.com>
2022-10-16 16:18:32 +0300

    fix key and library

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { start } from './js/start-func';
+import './js/modal-log-in';
+import './js/log-in';
 import './js/pagination';
 import './js/watchedLib';
 import './js/modal_film';
 import './js/themeSwitcher';
 import './js/spinner';
-import './js/modal-log-in';
-import './js/log-in';
 import './js/scroll';
 import './js/footer-modal';
 start();
