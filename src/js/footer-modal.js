14954c5d56cd9d36cc70f1859b0467ef947683de
Volkova Valeria <vvolk.valeria@gmail.com>
2022-10-16 21:37:40 +0300

    changeFooterModalTheme

--- src/js/footer-modal.js
+++ src/js/footer-modal.js
@@ -1,6 +1,6 @@
 
 import { openModalFooterEl, backdropEl } from "./refs";
-import { resetMarkup,  getTeamMembers, renderModalAboutTeam} from "./footer-modal-markup";
+import { resetMarkup,  getTeamMembers, renderModalAboutTeam, changeFooterModalTheme } from "./footer-modal-markup";
 
 openModalFooterEl.addEventListener('click', onCreationAndOpenFooterModal);
 
@@ -76,6 +76,9 @@ async function onCreationAndOpenFooterModal() {
     renderModalAboutTeam();
     toggleBackdrop();
     onStopScroll();
+    changeFooterModalTheme();
     getTeamMembers();
     setTimeout(onOpenFooterModal, 300);
 }
+
+
