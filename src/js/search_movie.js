90c0a6fc3deb8468c40722b9f5be6d1e7dff5703
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-10-16 09:37:51 +0300

    Удалил не рабочий функционал спиннера
    Удалил не рабочий функционал спиннера и всплывающих сообщений инпута

--- src/js/search_movie.js
+++ src/js/search_movie.js
@@ -10,7 +10,7 @@ const axios = require('axios').default;
 
 import Notiflix from 'notiflix';
 
-import debounce from 'lodash.debounce';
+
 
 import { renderMoviesTrending } from './renderMoviesTrending';
 
@@ -19,52 +19,13 @@ const searchFilmInput = document.querySelector('.header__form-input');
 const moviesElement = document.querySelector('.movies');
 
 
-const DEBOUNCE_DELAY = 300;
+
 
 let inputValue = '';
 let searchFilms = true;
 // const URL_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&language=en-US&query=${inputValue}&page=1&include_adult=false`;
 
-// Применяем приём Debounce (задержка в 300 мс)
-searchFilmInput.addEventListener('input',
-  debounce(onInputChenge, DEBOUNCE_DELAY));
 
-// Санитизация введённой строки методом trim()
-async function onInputChenge() {
-  const page = searchFilmInput.value.trim();
-  if (page === '') {
-    return (moviesElement.innerHTML = '');
-   
-  }
-
-  const data = await fetchFilms(page);
-  console.log(data);
-  moviesElement.innerHTML = '';
-  
-
-  try {
-    // Интерфейс с помощью бибилиотеки Notiflix
-    if (data.length > 10) {
-      Notiflix.Notify.info(
-        'Too many matches found. Please enter a more specific movie name.'
-      );
-    } else if (data.length < 10 && data.length >= 2) {
-      moviesElement.insertAdjacentHTML(
-        'beforeend',
-        card(data)
-      );
-    } else {
-      moviesElement.insertAdjacentHTML(
-        'beforeend',
-        card(data)
-      );
-    }
-  }
-  catch {
-    Notiflix.Notify.failure('Oops, there is no movie with that name');
-        return [];
-  }
-}
 
 
 export async function fetchFilms(page) {
@@ -75,7 +36,8 @@ export async function fetchFilms(page) {
     searchFilms = false;
     if (page === 1) {
       pagination.reset(fetchResult.data.total_results);
-    }
+    } 
+   
     console.log(fetchResult.data);
     return fetchResult.data.results;
   } catch (error) {
