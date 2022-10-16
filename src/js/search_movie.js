cfc1795becc439807c672f7af5a8e3629a8e2e3a
ViktorPonomarev <viktorponomarev15.02.89@gmail.com>
2022-10-16 18:15:32 +0300

    мрио

--- src/js/search_movie.js
+++ src/js/search_movie.js
@@ -6,9 +6,9 @@ import { renderMoviesTrending } from './renderMoviesTrending';
 
 import { Loading } from 'notiflix/build/notiflix-loading-aio';
 
-import { loader } from './refs';
 
-const axios = require('axios').default;
+
+
 
 import Notiflix from 'notiflix';
 
@@ -21,20 +21,13 @@ const searchFilmInput = document.querySelector('.header__form-input');
 const moviesElement = document.querySelector('.movies');
 
 
-searchFilmInput.addEventListener("input", onTexterialInput);
-
 let inputValue = '';
 let searchFilms = true;
 
 
 
-// const URL_SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&language=en-US&query=${inputValue}&page=1&include_adult=false`;
-
-
-  
-
 export async function fetchFilms(page) {
-  
+
   try {
     const fetchResult = await axios.get(
       `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&language=en-US&query=${inputValue}&page=${page}&include_adult=false`
@@ -44,79 +37,66 @@ export async function fetchFilms(page) {
     searchFilms = false;
     if (page === 1) {
       pagination.reset(fetchResult.data.total_results);
-    }else if (!data || data.length === 0) {
+    } else if (!fetchResult.data || fetchResult.data.length === 0) {
+      Loading.remove();
         Notiflix.Notify.failure(
                 'Search result not successful. Enter the correct movie name and try again.'
-            );
+      );
+      
           } 
     
           console.log(fetchResult.data);
           return fetchResult.data.results;
   } catch (error) {
-    Loading.remove();
-           Notiflix.Notify.failure('You havent added any movies yet. Please use search to find relevant movies.');
+    console.log(error);
+           Notiflix.Notify.failure('Search result not successful. Enter the correct movie name and try again.');
               return [];
   }
   
-      }
-   
-   
-
-async function searchFilm(e) {
-  e.preventDefault();
-
-  
-  
-  inputValue = searchFilmInput.value;
-  console.log(searchFilmInput.value);
-  searchFilmForm.reset();
-  moviesElement.innerHTML = '';
-  await renderMoviesTrending(fetchFilms(page));
-  // loader.classList.add('loader-hidden');
-  
-  
- 
-//  Loading.hourglass({
-//             svgColor: "rgb(255, 106, 0)",
-//             backgroundColor: "rgba(0,0,0,0)", 
-             
-//         });
-     
-
-    
-   
-  // Всплывающие уведомления с помощью Notiflix
-  // try {
-        
-  //       if (total_results.page === 0) {
-  //           Notiflix.Notify.info(
-  //               'You havent added any movies yet. Please use search to find relevant movies.'
-  //           );
-  //       }
-  //   }
-
-  //   catch {
-
-  //       Notiflix.Notify.failure('Search result not successful. Enter the correct movie name and try again.');
-  //       return [];
-           
-  //   }   
-    
 }
 
 
+      async function searchFilm(e) {
+        e.preventDefault();
+      
+        
+        
+        inputValue = searchFilmInput.value;
+        console.log(searchFilmInput.value);
+        searchFilmForm.reset();
+        moviesElement.innerHTML = '';
+        await renderMoviesTrending(fetchFilms(page));
+      }
 
-pagination.on('afterMove', event => {
-  const currentPage = event.page;
-  moviesElement.innerHTML = '';
-  if (searchFilms) {
-    renderMoviesTrending(fetchMoviesTrending(currentPage));
-  } else {
-    renderMoviesTrending(fetchFilms(currentPage));
-  }
-  onScroll();
-  onToTopBtn();
-});
 
-searchFilmForm.addEventListener('submit', searchFilm);
+      pagination.on('afterMove', event => {
+        const currentPage = event.page;
+        moviesElement.innerHTML = '';
+        if (searchFilms) {
+          renderMoviesTrending(fetchMoviesTrending(currentPage));
+        } else {
+          renderMoviesTrending(fetchFilms(currentPage));
+        }
+        onScroll();
+        onToTopBtn();
+      });
+      
+      searchFilmForm.addEventListener('submit', searchFilm);
+
+  
+
+   
+   
+
+  
+  
+  
+
+
+    
+   
+ 
+
+
+
 
